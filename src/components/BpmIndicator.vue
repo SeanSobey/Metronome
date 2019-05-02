3d09fd7bb27c932d494c005f9abbbc767e6e838e
Sean Sobey SAHL <seans@sahomeloans.com>
2019-05-02 17:23:46 +0200

    WIP BPM indicator

---
+++ src/components/BpmIndicator.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<b-badge pill variant="primary" class="mx-2">1</b-badge>
+		<b-badge pill variant="primary" class="mx-2">2</b-badge>
+		<b-badge pill variant="primary" class="mx-2">3</b-badge>
+		<b-badge pill variant="primary" class="mx-2">4</b-badge>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class BpmIndicator extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
