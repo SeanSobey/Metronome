164caf2e0e19d5a008a2d8809a1286e580c108ce
Sean Sobey SAHL <seans@sahomeloans.com>
2019-05-21 10:17:35 +0200

    Fixed devServer update issue

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
 	publicPath: isProd ? '/Metronome/' : '/',
+	devServer: {
+		clientLogLevel: 'silent', // ValidationError: webpack Dev Server Invalid Options :(
+	},
 	configureWebpack: (webpackConfig) => {
 		if (!isProd) {
 			webpackConfig.devtool = 'source-map';
